e407ebd083d25e0fdcf665856df28f38945f706f
Andrea Franz <andrea@gravityblast.com>
2024-12-05 15:12:09 +0100

    fix vote counts rendering

--- app/helpers/ballots_helper.rb
+++ app/helpers/ballots_helper.rb
@@ -3,8 +3,8 @@ module BallotsHelper
     votes_count = 0
     if votes && question_votes = votes.data[question_id.to_s]
       votes_count = question_votes[type.to_s].to_i
-      render "votes_count_fields", question_id: question_id, type: type, votes_count: votes_count
     end
+    render "votes_count_fields", question_id: question_id, type: type, votes_count: votes_count
   end
 
   def format_end_date(end_date)
