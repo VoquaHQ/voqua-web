5c5d7a3b09778f20c764068b95e380f10bad2a74
Erich <leonerichsen@gmail.com>
2024-12-09 23:24:02 +0000

    imporve header and add x and substack

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,13 @@
-// Import and register all your controllers from the importmap under controllers/*
+import { application } from "./application"
 
-import { application } from "controllers/application"
+import MenuController from "./menu_controller"
+application.register("menu", MenuController)
 
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import AlertController from "./alert_controller"
+application.register("alert", AlertController)
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+import OnboardingController from "./onboarding_controller"
+application.register("onboarding", OnboardingController)
+
+import VotingController from "./voting_controller"
+application.register("voting", VotingController)
