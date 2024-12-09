8dd1f3c2218d241aaf16ac84c8db6eb90b09cff5
Andrea Franz <andrea@gravityblast.com>
2024-12-09 22:12:51 +0100

    prod mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,19 @@ Rails.application.configure do
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "qv_production"
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "voqua-d85d9f43aea7.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -94,6 +106,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.action_mailer.default_url_options = { host: "qv.local" }
 end
