9752185f6c7dad6bd25b4e690d5d5684b3be0701
Erich <leonerichsen@gmail.com>
2024-12-22 19:59:38 +0000

    make copy button simpler

--- app/javascript/controllers/copy_results_controller.js
+++ app/javascript/controllers/copy_results_controller.js
@@ -6,18 +6,12 @@ export default class extends Controller {
   copy() {
     const text = this.sourceTarget.value
     navigator.clipboard.writeText(text).then(() => {
-      // Show feedback
-      const originalText = this.buttonTarget.textContent
-      this.buttonTarget.textContent = "Copied!"
-      this.buttonTarget.classList.remove("bg-blue-600", "hover:bg-blue-700")
-      this.buttonTarget.classList.add("bg-green-600")
-
-      // Reset after 2 seconds
+      const button = this.buttonTarget
+      button.textContent = "Copied!"
+      
       setTimeout(() => {
-        this.buttonTarget.textContent = originalText
-        this.buttonTarget.classList.remove("bg-green-600")
-        this.buttonTarget.classList.add("bg-blue-600", "hover:bg-blue-700")
-      }, 2000)
+        button.textContent = "Copy results to clipboard"
+      }, 1500)
     })
   }
 }
