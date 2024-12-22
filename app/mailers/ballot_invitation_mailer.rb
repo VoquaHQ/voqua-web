79788998396fd13f22d4910b5e78d63edf9ec37a
Erich <leonerichsen@gmail.com>
2024-12-22 20:15:03 +0000

    include admin email and title in invites to votes

--- app/mailers/ballot_invitation_mailer.rb
+++ app/mailers/ballot_invitation_mailer.rb
@@ -2,6 +2,7 @@ class BallotInvitationMailer < ApplicationMailer
   def invite(ballot, invitation)
     @ballot = ballot
     @invitation = invitation
+    @inviter_email = ballot.profile.user.email
     @url = ballot_accept_invitation_url(@ballot, token: invitation.token)
     mail(
       to: invitation.email,
