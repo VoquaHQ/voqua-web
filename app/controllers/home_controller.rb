2d5ea9651f5bb4304266d91876de4ff1888bb065
Erich <leonerichsen@gmail.com>
2024-12-28 18:24:19 +0000

    improve nav bar dynamic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
+    redirect_to my_root_path if current_user
   end
 
   def beta
+    redirect_to my_root_path if current_user
   end
 end
