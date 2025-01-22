cd666b66a2c06e67016833809790295a9cad5035
Andrea Franz <andrea@gravityblast.com>
2025-01-22 16:44:39 +0100

    set email from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Voqua <notifications@mailer2.voqua.io>"
+  default from: "Voqua <notifications@mailer.voqua.io>"
   layout "mailer"
 end
