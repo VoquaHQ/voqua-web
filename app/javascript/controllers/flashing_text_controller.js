94db6f5bf5213fd25461516c9c2164866deceab6
Andrea Franz <andrea@gravityblast.com>
2025-04-01 15:29:36 +0200

    remove unused controllers

--- app/javascript/controllers/flashing_text_controller.js
+++ app/javascript/controllers/flashing_text_controller.js
@@ -1,30 +1,30 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    interval: { type: Number, default: 300 }
-  }
+    interval: { type: Number, default: 300 },
+  };
 
   connect() {
-    this.visible = true
-    this.startFlashing()
+    this.visible = true;
+    this.startFlashing();
   }
 
   disconnect() {
-    this.stopFlashing()
+    this.stopFlashing();
   }
 
   startFlashing() {
     this.flashingInterval = setInterval(() => {
-      this.visible = !this.visible
-      this.element.classList.toggle('opacity-30', !this.visible)
-      this.element.classList.toggle('opacity-100', this.visible)
-    }, this.intervalValue)
+      this.visible = !this.visible;
+      this.element.classList.toggle("opacity-30", !this.visible);
+      this.element.classList.toggle("opacity-100", this.visible);
+    }, this.intervalValue);
   }
 
   stopFlashing() {
     if (this.flashingInterval) {
-      clearInterval(this.flashingInterval)
+      clearInterval(this.flashingInterval);
     }
   }
 }
